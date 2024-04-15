951db17ff44264cb249499ec98f241983acfa501
Neco86 <33189641+Neco86@users.noreply.github.com>
2024-04-15 17:19:04 +0800

    Add files via upload

--- compare.js
+++ compare.js
@@ -1,29 +1,32 @@
 (() => {
-    fetch('https://neco86.github.io/svg-measure/')
-        .then(res => res.text())
-        .then(html => {
-            const blob = new Blob([html], { type: 'text/html' });
-            return URL.createObjectURL(blob);
-        })
-        .then(url => {
-            const iframe = document.createElement('iframe');
-            const styles = {
-                position: 'fixed',
-                zIndex: '10000',
-                top: '0px',
-                left: '0px',
-                width: '100vw',
-                height: '100vh',
-                border: 'none',
-            };
-            Object.keys(styles).forEach(key => {
-                iframe.style[key] = styles[key];
-            });
-            iframe.src = url;
-            document.body.appendChild(iframe);
-        })
+    if (!top.SVG_MEASURE_COMPARE) {
+        top.SVG_MEASURE_COMPARE = true;
+        fetch('https://neco86.github.io/svg-measure/')
+            .then(res => res.text())
+            .then(html => {
+                const blob = new Blob([html], { type: 'text/html' });
+                return URL.createObjectURL(blob);
+            })
+            .then(url => {
+                const iframe = document.createElement('iframe');
+                const styles = {
+                    position: 'fixed',
+                    zIndex: '10000',
+                    top: '0px',
+                    left: '0px',
+                    width: '100vw',
+                    height: '100vh',
+                    border: 'none',
+                };
+                Object.keys(styles).forEach(key => {
+                    iframe.style[key] = styles[key];
+                });
+                iframe.src = url;
+                document.body.appendChild(iframe);
+            })
 
-    window.onbeforeunload = e => {
-        e.preventDefault();
+        window.onbeforeunload = e => {
+            e.preventDefault();
+        }
     }
 })()
